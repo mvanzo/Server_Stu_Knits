327e420ef3bcb8291857fd4fdd7aa7ff3af860dc
mvanzo <97002794+mvanzo@users.noreply.github.com>
2022-04-15 15:22:32 -0700

    axios request for order history

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express')
 const cors = require('cors')
 const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY)
 
+const axios = require('axios')
+
 const app = express()
 const PORT = 3002
 
@@ -19,6 +21,24 @@ app.use(express.json())
 app.get('/', (req, res)=> {
     res.json({msg:'welcome to the user app'})
 })
+
+
+const config = {
+    headers: { Authorization: `Bearer ${process.env.STRIPE_PRIVATE_KEY}` }
+};
+
+app.get('/findorders', async (req, res)=> {
+    try{
+        // let findOrders = await axios.get('https://api.stripe.com/v1/checkout/sessions', config)
+        let findOrders = await axios.get('https://api.stripe.com/v1/charges', config)
+        res.json(findOrders.data.data)
+        // console.log(findOrders.data)
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+
 
 // controllers
 app.use('/api-v1/users', require('./controllers/api-v1/users'))
